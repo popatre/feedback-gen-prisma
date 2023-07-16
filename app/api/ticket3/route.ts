0111b3970b9c9fa3be81bbd1ffe5ac19a0506f80
Popatre <jonny_m111@hotmail.com>
2023-07-16 19:27:46 +0100

    add guidance to ticket 3

--- app/api/ticket3/route.ts
+++ app/api/ticket3/route.ts
@@ -31,33 +31,33 @@ export async function GET() {
             {
                 guidance:
                     " Consideration of state (the key states needed and levels, i.e. articles/topics), can always be added to later but good to see them thinking about it as early as possible",
-                www: "nice one",
-                ebi: "rubbish stuff",
+                www: "Good that you've thought about the states in the component tree",
+                ebi: "We be great for you to think about your states and where they will live",
             },
             {
                 guidance: "Wireframes should be mobile first",
-                www: "nice one",
-                ebi: "rubbish stuff",
+                www: "Nice mobile first designs! More user will view your app on a mobile, so excellent to think about their experience first",
+                ebi: "Ideally, your designs should be mobile first. More user will view your app on a mobile, so excellent to think about their experience first. The mobile design will also be easier to make and you'll find a lot of the default css styling is what you want to achieve your design",
             },
             {
                 guidance:
                     "Should not be over ambitious, if the design is overly complicated for the students ability then encourage them to simplify it as they can always add more features later",
-                www: "nice one",
-                ebi: "rubbish stuff",
+                www: "Nice design that isn't overly ambitious",
+                ebi: "There's quite a lot of functionality planned here. I'd narrow it down a bit and get the basic app working; you can always add fancier stuff in later if you get the basic app done",
             },
         ],
         couldData: [
             {
                 guidance:
                     "could use any visual tool of their choice. Figma, Excalidraw, JS Paint, paper and pencil.",
-                www: "nice one",
-                ebi: "rubbish stuffffff",
+                www: "You've used a design tool to make your plan",
+                ebi: "Create a plan using excalidraw, figma, jsPaint or even paper and pencil",
             },
             {
                 guidance:
                     "some thought on design prototyping. Colour, fonts etc.",
-                www: "nice one",
-                ebi: "rubbish stuffffff",
+                www: "Nice that you've thought about the designs/colours/fonts before starting",
+                ebi: "Optional: Could think about the designs/font/colours before starting",
             },
         ],
     };
