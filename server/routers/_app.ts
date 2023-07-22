dd7a2d0d04bea1ebd8f2e0994bd6a1f1045bc7aa
Popatre <jonny_m111@hotmail.com>
2023-07-22 15:58:54 +0100

    add ticket router and get tickets route

--- server/routers/_app.ts
+++ server/routers/_app.ts
@@ -1,8 +1,10 @@
 import { publicProcedure, router } from "../trpc";
 import { blockRouter } from "./block.router";
+import { ticketRouter } from "./ticket.router";
 
 export const appRouter = router({
     block: blockRouter,
+    ticket: ticketRouter,
 });
 
 export type AppRouter = typeof appRouter;
