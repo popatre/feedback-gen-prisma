da464c4757735ed329780cd650a8902931e81f94
Popatre <jonny_m111@hotmail.com>
2023-07-22 08:17:07 +0100

    add function and test to get single block info

--- models/block.model.ts
+++ models/block.model.ts
@@ -1,6 +1,20 @@
+import { Block } from "@prisma/client";
 import prisma from "../db/connection";
 
-export const selectAllBlocks = async () => {
+export const selectAllBlocks = async (): Promise<Block[]> => {
     const blocks = await prisma.block.findMany();
     return blocks;
+};
+
+export const selectSingleBlock = async (blockName: string) => {
+    const block = await prisma.block.findUnique({
+        where: {
+            block_name: blockName,
+        },
+        include: {
+            tickets: true,
+        },
+    });
+
+    return block;
 };
