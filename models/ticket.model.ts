8fe5c031deed06733d87b496c1ba03c09df1a809
Popatre <jonny_m111@hotmail.com>
2023-07-22 12:20:20 +0100

    add test for ticket feedback from email

--- models/ticket.model.ts
+++ models/ticket.model.ts
@@ -1,14 +1,26 @@
 import { Ticket, Block } from "@prisma/client";
 import prisma from "../db/connection";
 
-export const selectTicketById = async (ticketId: string) => {
+export const selectTicketByIdWithEmail = async (
+    ticketId: string,
+    userEmail: string
+) => {
     const ticket = await prisma.ticket.findUnique({
         where: {
             ticket_id: ticketId,
         },
         include: {
-            guidance: true,
+            guidance: {
+                include: {
+                    feedback: {
+                        where: {
+                            user_email: userEmail,
+                        },
+                    },
+                },
+            },
         },
     });
+
     return ticket;
 };
