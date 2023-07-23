b0ac23c285a880360851c522f2bd6ecefea17627
Popatre <jonny_m111@hotmail.com>
2023-07-23 08:11:16 +0100

    add get ids utils

---
+++ __tests__/utils/getTableIds.test.ts
@@ -0,0 +1,21 @@
+import getTableIds from "../../db/utils/getTableIds";
+import * as data from "../../db/data/index";
+import seed from "../../db/seeds/seed";
+
+beforeEach(() => seed(data));
+
+describe("getTableIds", () => {
+    test("should return ids for selected table", async () => {
+        const tableIds: [] = await getTableIds("Ticket", "ticket_id");
+        expect(tableIds).toBeInstanceOf(Array);
+        expect(tableIds).toHaveLength(5);
+        tableIds.forEach((element) => {
+            expect(element).toMatchObject({ ticket_id: expect.any(String) });
+        });
+    });
+    test("should handle non valid columns", async () => {
+        const tableIds: [] = await getTableIds("Ticket", "not_column");
+        expect(tableIds).toBeInstanceOf(Array);
+        expect(tableIds).toHaveLength(0);
+    });
+});
