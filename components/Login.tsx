2de49193f5cf0ce8cc731399dbba2038d728a7da
Popatre <jonny_m111@hotmail.com>
2023-07-24 14:20:39 +0100

    styling login button

--- components/Login.tsx
+++ components/Login.tsx
@@ -16,14 +16,15 @@ export default function Login() {
     };
 
     return (
-        <section className="flex flex-col items-center justify-center min-h-screen">
-            <div className="bg-white shadow-md rounded px-8 py-6 max-w-xs w-full">
+        <section className="flex flex-col items-center  min-h-[65vh] justify-center">
+            <div className="bg-white shadow-md rounded px-8 py-20 max-w-[600px] w-[80%] w-full flex items-center flex-col">
                 <h2 className="text-2xl font-bold mb-6">Sign in with Google</h2>
                 <button
                     onClick={handleSignIn}
-                    className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded w-full"
+                    className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded w-[80%] flex justify-center items-center"
                 >
-                    Login <FcGoogle size="40" />
+                    <p className="mr-5">Login </p>
+                    <FcGoogle size="40" />
                 </button>
             </div>
         </section>
