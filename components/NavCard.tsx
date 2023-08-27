1da74004f48e037e29bbd91df9c4a0ec5fe95183
Popatre <jonny_m111@hotmail.com>
2023-08-27 10:47:40 +0100

    remove make height causing overflow

--- components/NavCard.tsx
+++ components/NavCard.tsx
@@ -6,7 +6,7 @@ type Props = { description: string; url: string };
 export default function NavCard({ description, url }: Props) {
     return (
         <Link href={url}>
-            <article className="nav-card border-solid border-2  rounded-md p-10 h-[100%] max-h-[200px] shadow-md hover:bg-sky-300 flex justify-left items-center ">
+            <article className="nav-card border-solid border-2 rounded-md p-10 h-[100%]  shadow-md hover:bg-sky-300 flex justify-left items-center ">
                 <p className="text-white font-semibold">{description}</p>
             </article>
         </Link>
