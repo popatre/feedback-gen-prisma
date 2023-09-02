76d21e6010c1ce0287b31c2d374e3ea6bde8c5ec
popatre <76402245+popatre@users.noreply.github.com>
2023-09-02 14:46:16 +0100

    add css to stop home page boxes spanning whole screen

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import NavCard from "@/components/NavCard";
 
 export default function Home() {
     return (
-        <main className="pt-20 grid mx-20 gap-10 grid-cols-2">
+        <main className="pt-20 grid  gap-10 grid-cols-2 max-w-7xl w-10/12 ml-auto mr-auto">
             <NavCard url="/be" description="Back End" />
             <NavCard url="/fe" description="Front End" />
         </main>
