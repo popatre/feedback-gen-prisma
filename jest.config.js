2e6e4195eba38ef15acd654ed94cb421e7b61a1b
popatre <76402245+popatre@users.noreply.github.com>
2023-09-02 18:56:09 +0100

    setup RTL

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
-module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+    dir: "./",
+});
+
+const customJestConfig = {
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+    moduleDirectories: ["node_modules", "<rootDir>/"],
+    testEnvironment: "jest-environment-jsdom",
 };
+
+module.exports = createJestConfig(customJestConfig);
